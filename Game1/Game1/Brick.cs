e10af6c4e4c59555d110141625b15f308000421e
cube2166 <cube_leony@hotmail.com>
2017-05-04 15:48:15 +0800

    第一版,只有碰撞測試

---
+++ Game1/Game1/Brick.cs
@@ -0,0 +1,321 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+
+namespace Game1
+{
+    public class MyObject
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public object texture { get; set; }
+        public Color cc { get; set; }
+
+        public bool canCheck { get; set; }
+        public bool canBreak { get; set; }
+        public bool canReflection { get; set; }
+
+        public event Action<object> CheckEvent;
+        public event Action<object> BreakEvent;
+        public event Func<object, object, bool> ReflectionEvent;
+
+        public void OnBreak()
+        {
+            if(BreakEvent != null)
+            {
+                BreakEvent(this);
+            }
+        }
+
+        public void OnCheck()
+        {
+            CheckEvent.Invoke(this);
+        }
+
+        public bool OnReflection(object obj)
+        {
+            if(ReflectionEvent != null)
+            {
+                return ReflectionEvent(this, obj);
+            }
+            return false;
+        }
+
+        public MyObject(object tt)
+        {
+            texture = tt;
+        }
+
+        public event Action<object,object> showHandler;
+        public void Show()
+        {
+            showHandler.Invoke(texture, this);
+        }
+    }
+
+    public class MyBall : MyObject
+    {
+        private float Speed { get; set; }
+        private double _degree;
+        private double Degree
+        {
+            get { return _degree; }
+            set
+            {
+                if(_degree != value)
+                {
+                    _degree = value;
+                    vx = Speed * Math.Cos(radians(_degree));
+                    vy = Speed * Math.Sin(radians(_degree));
+                }
+            }
+        }
+
+        private double radians(double deg)
+        {
+            return deg * (Math.PI / 180);
+        }
+
+        private double vx;
+        private double vy;
+        public MyBall(int x, int y, int size, float sp, Color c, object tt) : base(tt)
+        {
+            X = x;
+            Y = y;
+            Width = size;
+            Height = size;
+            cc = c;
+            Speed = sp;
+
+            Degree = 45;
+
+            canCheck = true;
+            canBreak = false;
+            canReflection = false;
+        }
+
+        public void Reflection(int val)
+        {
+            switch (val)
+            {
+                case 1:
+                    Degree = -Degree;
+                    break;
+                case 2:
+                    Degree = 180 - Degree;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void Update(float elapsedTime)
+        {
+            X += (float)(vx * elapsedTime);
+            Y += (float)(vy * elapsedTime);
+            OnCheck();
+        }
+    }
+
+    public class MyBoard : MyObject
+    {
+        private int BoundL { get; set; }
+        private int BoundR { get; set; }
+        private int BoundU { get; set; }
+        private int BoundD { get; set; }
+        private float Speed { get; set; }
+        public MyBoard(int x, int y, int w, int h, Color c, float sp, int bl, int br, object tt): base(tt)
+        {
+            X = x;
+            Y = y;
+            Width = w;
+            Height = h;
+            cc = c;
+            this.Speed = sp;
+            this.BoundL = bl;
+            this.BoundR = br;
+
+            canCheck = false;
+            canBreak = false;
+            canReflection = true;
+        }
+
+        public void MoveLeft()
+        {
+            this.X -= (int)this.Speed;
+            if (X < BoundL) X = BoundL;
+        }
+
+        public void MoveRight()
+        {
+            this.X += (int)this.Speed;
+            if ((X+Width) > BoundR) X = BoundR-Width;
+        }
+    }
+
+    public class MyWall : MyObject
+    {
+        public MyWall(int x, int y, int w, int h, Color c, object tt) : base(tt)
+        {
+            this.X = x;
+            Y = y;
+            Width = w;
+            Height = h;
+            cc = c;
+
+            canCheck = false;
+            canBreak = false;
+            canReflection = true;
+        }
+    }
+
+    public class MyFont : MyObject
+    {
+        public String ss { get; set; }
+        public readonly int sX;
+        public readonly int sY;
+        public readonly Color sC;
+        public MyFont(int x, int y, string s, Color c, int offset, object tt) : base(tt)
+        {
+            X = x;
+            sX = x + offset;
+            Y = y;
+            sY = y + offset;
+            cc = c;
+            sC = new Color((byte)(cc.R * 0.5), (byte)(cc.G * 0.5), (byte)(cc.B * 0.5), (byte)(cc.A * 0.5));
+            ss = s;
+
+            canCheck = false;
+            canBreak = false;
+            canReflection = false;
+        }
+    }
+
+    public class MyBrick: MyObject
+    {
+        public MyBrick(int x, int y, int w, int h, Color c, object tt) : base(tt)
+        {
+            this.X = x;
+            Y = y;
+            Width = w;
+            Height = h;
+            cc = c;
+
+            canCheck = false;
+            canBreak = true;
+            canReflection = true;
+        }
+    }
+
+    public class MyObjList : ObservableCollection<MyObject>
+    {
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            if(e.Action == NotifyCollectionChangedAction.Add)
+            {
+                foreach (var item in e.NewItems)
+                {
+                    MyObject temp = item as MyObject;
+                    if (temp == null) continue;
+                    
+                    if(temp.canBreak == true)
+                    {
+                        temp.BreakEvent += Temp_BreakEvent;
+                    }
+                    if (temp.canCheck == true)
+                    {
+                        temp.CheckEvent += Temp_CheckEvent;
+                    }
+                    if (temp.canReflection == true)
+                    {
+                        temp.ReflectionEvent += Temp_ReflectionEvent;
+                    }
+                }
+            }
+        }
+
+        private bool Temp_ReflectionEvent(object src, object dist)
+        {
+            MyBall ball = dist as MyBall;
+            MyObject trig = src as MyObject;
+            if (ball == null || trig == null) return false;
+
+            Vector2 pointL, pointR, pointT, pointB;
+            pointL.X = ball.X;
+            pointL.Y = ball.Y + ball.Height / 2;
+            pointR.X = ball.X + ball.Width;
+            pointR.Y = ball.Y + ball.Height / 2;
+
+            pointT.X = ball.X + ball.Width / 2;
+            pointT.Y = ball.Y;
+            pointB.X = ball.X + ball.Width / 2;
+            pointB.Y = ball.Y + ball.Height;
+
+
+
+            if ( (pointL.X > trig.X) && (pointL.X <= trig.X+trig.Width) && 
+                (pointL.Y > trig.Y) && (pointL.Y <= trig.Y+trig.Height) )
+            {
+                ball.Reflection(2);
+                ball.X += (trig.X + trig.Width) - (int)pointL.X;
+                return true;
+            }
+            else if ((pointR.X > trig.X) && (pointR.X <= trig.X + trig.Width) &&
+                (pointR.Y > trig.Y) && (pointR.Y <= trig.Y + trig.Height))
+            {
+                ball.Reflection(2);
+                ball.X += (trig.X) - (int)pointR.X;
+                return true;
+            }
+            else if ((pointT.X > trig.X) && (pointT.X <= trig.X + trig.Width) &&
+                (pointT.Y > trig.Y) && (pointT.Y <= trig.Y + trig.Height))
+            {
+                ball.Reflection(1);
+                ball.Y += (trig.Y + trig.Height) - (int)pointT.Y;
+                return true;
+            }
+            else if ((pointB.X > trig.X) && (pointB.X <= trig.X + trig.Width) &&
+                (pointB.Y > trig.Y) && (pointB.Y <= trig.Y + trig.Height))
+            {
+                ball.Reflection(1);
+                ball.Y += (trig.Y) - (int)pointB.Y;
+                return true;
+            }
+            return false;
+
+        }
+
+        private void Temp_CheckEvent(object obj)
+        {
+            MyObject ball = obj as MyObject; //球
+            if (ball == null) return;
+            //檢查碰撞對象
+            foreach (var item in this)
+            {
+                //檢查能否反射球
+                bool tempBool =  item.OnReflection(ball);
+
+                //如果有碰撞發生
+                if (tempBool == true)
+                {
+                    //檢查能否消失
+                    item.OnBreak();
+                    break;
+                }
+            }
+
+        }
+
+        private void Temp_BreakEvent(object obj)
+        {
+            this.Remove((MyObject)obj);
+        }
+    }
+}
---
+++ Game1/Game1/Game1.cs
@@ -0,0 +1,198 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+namespace Game1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D blank;
+        Texture2D bb1;
+        Texture2D bb2;
+        Texture2D ball;
+        Texture2D[] brickArray;
+        SpriteFont font;
+        MyBoard player;
+        MyBall mb;
+        MyObjList myObjectCollect;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferHeight = 600;
+            graphics.PreferredBackBufferWidth = 800;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            IsMouseVisible = true;
+
+            base.Initialize();
+        }
+
+        void prepareShow(object obj1, object obj2)
+        {
+            Texture2D temp = obj1 as Texture2D;
+            MyObject temp2 = obj2 as MyObject;
+//            Vector2 spritePosition = new Vector2(temp2.X, temp2.Y);
+            spriteBatch.Draw(temp, new Rectangle((int)temp2.X, (int)temp2.Y, temp2.Width, temp2.Height), temp2.cc);
+ //           spriteBatch.Draw(temp, spritePosition, new Rectangle((int)temp2.X, (int)temp2.Y, temp2.Width, temp2.Height), null, temp2.cc);
+        }
+
+        void prepareShowFont(object obj1, object obj2)
+        {
+            SpriteFont temp = obj1 as SpriteFont;
+            MyFont temp2 = obj2 as MyFont;
+            spriteBatch.DrawString(temp, temp2.ss, new Vector2(temp2.sX, temp2.sY), temp2.sC);
+            spriteBatch.DrawString(temp, temp2.ss, new Vector2(temp2.X, temp2.Y), temp2.cc);
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            blank = Content.Load<Texture2D>("blank");
+            font = Content.Load<SpriteFont>("font");
+            ball = Content.Load<Texture2D>("ball");
+            brickArray = new Texture2D[4];
+            for (int ii = 0; ii < brickArray.Length; ii++)
+            {
+                string ss = string.Format("b{0}", ii + 1);
+                brickArray[ii] = Content.Load<Texture2D>(ss);
+            }
+            // TODO: use this.Content to load your game content here
+
+
+            MyWall[] wallCollect;
+            MyFont gameTitle;
+
+            int offset = 600 + (800 - 600) / 2 - (int)font.MeasureString("Brick - Breaking").X / 2;
+            gameTitle = new MyFont(offset, 20, "Brick - Breaking", Color.Yellow, -1,font);
+            gameTitle.showHandler += prepareShowFont;
+
+            player = new MyBoard(600 / 2 - 100 / 2, 600 - 20 - 10, 100, 20, Color.White, 5, 0 + 20, 600 - 20, blank);
+            player.showHandler += prepareShow;
+            wallCollect = new MyWall[3];
+            wallCollect[0] = new MyWall(0, 0, 600, 20, Color.Gray, blank);
+            wallCollect[0].showHandler += prepareShow;
+            wallCollect[1] = new MyWall(0, 0, 20, 600, Color.Gray, blank);
+            wallCollect[1].showHandler += prepareShow;
+            wallCollect[2] = new MyWall(600 - 20, 0, 20, 600, Color.Gray, blank);
+            wallCollect[2].showHandler += prepareShow;
+
+            myObjectCollect = new MyObjList();
+            myObjectCollect.Add(player);
+            myObjectCollect.Add(wallCollect[0]);
+            myObjectCollect.Add(wallCollect[1]);
+            myObjectCollect.Add(wallCollect[2]);
+            myObjectCollect.Add(gameTitle);
+
+            int maxCol = 10;
+            int maxRow = 10;
+            Random rr = new Random();
+
+            for (int row = 0; row < maxRow; row++)
+            {
+                int width = (600 - (20 * 2)) / maxCol;
+                int height = 30;
+                for (int col = 0; col < maxCol; col++)
+                {
+                    int px = ((width * col) + 20) + 1;
+                    int py = ((height * row) + 20) + 1;
+                    int ww = width - 2;
+                    int hh = height - 2;
+                    Texture2D text2D;
+                    text2D = brickArray[rr.Next(0, 3)];
+
+                    MyBrick temp;
+                    temp = new MyBrick(px, py, ww, hh, Color.White, text2D);
+                    temp.showHandler += prepareShow;
+                    myObjectCollect.Add(temp);
+                }
+            }
+            int size = 20;
+
+ //           mb = new MyBall((int)player.X + player.Width / 2 - size/2, (int)player.Y - size, size, 5, Color.Red, ball);
+            mb = new MyBall((int)player.X + 50, (int)player.Y - 50, size, 200, Color.Red, blank);
+            mb.showHandler += prepareShow;
+            myObjectCollect.Add(mb);
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            float elapsedTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            mb.Update(elapsedTime);
+
+            KeyboardState keyboard = Keyboard.GetState();
+            if (keyboard.IsKeyDown(Keys.A) || keyboard.IsKeyDown(Keys.Left))
+            {
+                player.MoveLeft();
+            }
+            else if (keyboard.IsKeyDown(Keys.D) || keyboard.IsKeyDown(Keys.Right))
+            {
+                player.MoveRight();
+            }
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            spriteBatch.Begin();
+
+            foreach (var item in myObjectCollect)
+            {
+                item.Show();
+            }
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
